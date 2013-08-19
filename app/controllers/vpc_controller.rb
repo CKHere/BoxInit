208808bacb0751924c07a56f9b1cca54e010661b
CKHere <cpkher@gmail.com>
2013-08-19 20:51:55 +0200

    Added support for relay

--- app/controllers/vpc_controller.rb
+++ app/controllers/vpc_controller.rb
@@ -1,3 +1,5 @@
+require 'socket'
+
 class VpcController < ApplicationController
   def index
   end
@@ -18,8 +20,33 @@ class VpcController < ApplicationController
     # end
   end
 
+  respond_to :html, :json
   def show
-    respond_with params 
+    port=4000+params[:id].to_i
+    puts "log: connect on " + port.to_s
+    clientSession = TCPSocket.new( "localhost", port )
+    puts "log: starting connection"
+    message = "ifInfo"
+    clientSession.puts message
+    ifInfo=clientSession.gets
+    clientSession.close
+    puts "log: connection closed"
+    ifs=ifInfo.split('|')
+    @interfaces = Hash.new
+    ifs.each { |s|
+      ifn,key,value=s.split('-')
+      if key=='MACaddr'
+        key='MAC Address'
+      elsif key=='IPaddr'
+        key='IP Address'
+      end
+      if not @interfaces[ifn]
+        @interfaces[ifn] = {key=>value}
+      else
+        @interfaces[ifn].merge!(key=>value)
+      end
+    }
+    puts "log: Interface values: " + @interfaces.to_s
   end
 
 end
